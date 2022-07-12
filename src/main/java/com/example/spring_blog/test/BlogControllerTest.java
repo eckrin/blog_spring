65dc841148d51d7ecfa0ad43490098f2a963decf
eckrin <bsh51515@naver.com>
2022-07-12 18:15:17 +0900

    basic settings

---
+++ src/main/java/com/example/spring_blog/test/BlogControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.spring_blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+
+    @GetMapping("/test/hello")
+    public String hello() {
+        return "<h1>hello spring boot</h1>";
+    }
+}
